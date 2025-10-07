8e8edf16ace015dc1c7d4ebef7b4dca05a28ff67
Debid Magar <linkwithdm@gmail.com>
2025-10-07 21:44:49 +0545

    add request-level rate limiting using Bucket4j Co-authored-by: thedebid <linkwithdm@gmail.com> Co-authored-by: sagar-subedi <sagaxic@gmail.com>

---
+++ src/main/java/np/com/debid/ipoinsightplusapi/service/RateLimiterService.java
@@ -0,0 +1,26 @@
+package np.com.debid.ipoinsightplusapi.service;
+
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+public class RateLimiterService {
+    private final Map<String, Bucket> cache = new ConcurrentHashMap<>();
+
+    public Bucket resolveBucket(String key) {
+        return cache.computeIfAbsent(key, this::newBucket);
+    }
+
+    private Bucket newBucket(String key) {
+        Bandwidth limit = Bandwidth.classic(5, Refill.greedy(5, Duration.ofMinutes(1)));
+        return Bucket.builder()
+                .addLimit(limit)
+                .build();
+    }
+}
